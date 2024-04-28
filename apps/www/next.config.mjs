2663064c5174a48fff939cb88cf8042b171bb40f
fagnersales <fs.fagnersales@gmail.com>
2024-04-28 16:37:07 -0300

    hello world

---
+++ apps/www/next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePacakages: ["@repo/ui"],
+};
+
+export default nextConfig;
---
+++ packages/ui/src/sum.tsx
@@ -0,0 +1,5 @@
+export function Sum(props: { a: number, b: number } ) {
+    return (
+        <div>The sum of {props.a} and {props.b} is: {props.a+props.b}</div>
+    )
+}
